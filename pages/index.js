b53b03e46f92801b6aa9c7a3a68a417170d666b2
Erik Bruce <74328681+ErikBruce12@users.noreply.github.com>
2024-12-21 18:26:22 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-   return <h1> Gigi ta em um site, slc (ou pelo menos no inicio dele.. eu acho) </h1>
+   return <h1> Gigi ta em um site, slc (ou pelo menos no inicio dele.. eu acho) :v </h1>
 }
 
 export default Home;
